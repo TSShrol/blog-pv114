8a6ae6496fc1075a842a593527c26d5a705a1ff7
TSShrol <shrolts@gmail.com>
2023-11-20 20:07:10 +0200

    added author of post

--- src/components/posts/PostsList.jsx
+++ src/components/posts/PostsList.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import AuthorPost from "./AuthorPost";
 
 const PostsList = () => {
     const posts = useSelector((store) => store.posts);
@@ -6,12 +7,17 @@ const PostsList = () => {
     return(
         <section>
             <h2>Posts</h2>
+            <hr/>
             {
             posts.map(post => (
+                
                 <article key={post.id}>
                     <h3>{post.title}</h3>
                     <p> {post.content.substring(0,50)}</p>
+                    <p> <AuthorPost userId={post.userId}/> </p>
+                    <hr/>
                 </article>
+                
             ))
             }
         </section>
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postsReducer from './appSlice/postsSlice';
+import  usersReducer from './appSlice/usersSlice';
 
 export const store = configureStore({
   reducer: {
-    posts: postsReducer
+    posts: postsReducer,
+    users: usersReducer 
   },
 });
